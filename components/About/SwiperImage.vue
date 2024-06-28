235acc960e1b5fe0486c41b0bf9948d07153a45a
Og'abek <ogabeksanoyev06@gmail.com>
2024-06-28 11:46:34 +0500

    .

--- components/About/SwiperImage.vue
+++ components/About/SwiperImage.vue
@@ -9,12 +9,12 @@
       </Swiper>
       <div class="hidden md:block absolute top-1/2 -translate-y-1/2 z-50 w-full">
          <div class="container flex items-center justify-between w-full">
-            <UIButton variant="outline" class="px-0 py-0 w-14 h-14 rounded-full bg-white/20 border-white/60" @click="categorySwiper.slidePrev()">
-               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <UIButton variant="outline" class="!px-0 !py-0 !w-14 !h-14 !rounded-full bg-white/20 border-white/60" @click="categorySwiper.slidePrev()">
+               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="w-20" viewBox="0 0 24 24" fill="none">
                   <path d="M14 7L10 12L14 17" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                </svg>
             </UIButton>
-            <UIButton variant="outline" class="px-0 py-0 w-14 h-14 rounded-full bg-white/20 border-white/60" @click="categorySwiper.slideNext()">
+            <UIButton variant="outline" class="!px-0 !py-0 !w-14 !h-14 !rounded-full bg-white/20 border-white/60" @click="categorySwiper.slideNext()">
                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                   <path d="M10 7L14 12L10 17" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                </svg>
